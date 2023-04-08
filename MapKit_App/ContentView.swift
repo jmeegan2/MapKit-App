06bad4f41191bfe719df70fa5b8f93917fffeab7
jmeegan2 <jmeegan8@gmail.com>
2023-04-08 16:18:29 -0400

    init

---
+++ MapKit_App/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  MapKit_App
+//
+//  Created by James Meegan on 4/8/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            //Baltimore, Maryland
+            latitude: 39.2904,
+            longitude: -76.6122
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.03)
+        )
+        var body: some View {
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.all)
+        }
+    
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ MapKit_App/MapKit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_AppApp.swift
+//  MapKit_App
+//
+//  Created by James Meegan on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MapView()
+        }
+    }
+}
